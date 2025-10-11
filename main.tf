e7e477d389729a0523a40e3c582db452527f9be5
Alok Raja <94987623+alokraja075@users.noreply.github.com>
2025-10-11 09:56:11 +0000

    update main.tf

--- main.tf
+++ main.tf
@@ -17,7 +17,6 @@ resource "aws_s3_bucket_ownership_controls" "ownership" {
 
 resource "aws_s3_bucket_public_access_block" "public_access" {
   bucket = aws_s3_bucket.terraformbucket.id
-
   block_public_acls       = false
   block_public_policy     = false
   ignore_public_acls      = false
