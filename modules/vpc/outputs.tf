94c1bbb3d832c9215669fe88c4999d4d18131971
alok raja <alokraja075@gmail.com>
2025-12-04 23:46:46 +0530

    add outputs for VPC ID, public and private subnet IDs, and NAT gateway IDs

---
+++ modules/vpc/outputs.tf
@@ -0,0 +1,19 @@
+output "vpc_id" {
+  description = "VPC ID"
+  value       = aws_vpc.this.id
+}
+
+output "public_subnet_ids" {
+  description = "List of public subnet IDs"
+  value       = [for s in aws_subnet.public : s.id]
+}
+
+output "private_subnet_ids" {
+  description = "List of private subnet IDs"
+  value       = [for s in aws_subnet.private : s.id]
+}
+
+output "nat_gateway_ids" {
+  description = "List of NAT gateway IDs (empty if not created)"
+  value       = var.enable_nat ? [for n in aws_nat_gateway.nat : n.id] : []
+}
