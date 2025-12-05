62c74be5eb3a2010acbf7eb9826e11bb6077c572
alok raja <alokraja075@gmail.com>
2025-12-05 23:01:49 +0530

    add variables for NAT gateway strategy, flow logs, and VPC endpoints for S3 and DynamoDB

--- modules/vpc/variables.tf
+++ modules/vpc/variables.tf
@@ -28,8 +28,56 @@ variable "private_subnet_cidrs" {
   default     = []
 }
 
-variable "enable_nat" {
-  description = "Whether to create NAT gateways for private subnets"
+variable "nat_gateway_strategy" {
+  description = "NAT gateway deployment strategy: \"none\" | \"single\" | \"per_az\""
+  type        = string
+  default     = "none"
+}
+
+variable "single_nat_subnet_index" {
+  description = "When using \"single\" nat_gateway_strategy, the public subnet index (0-based) to host the NAT gateway"
+  type        = number
+  default     = 0
+}
+
+variable "create_flow_logs" {
+  description = "Whether to create VPC Flow Logs"
+  type        = bool
+  default     = false
+}
+
+variable "flow_logs_traffic_type" {
+  description = "Flow logs traffic type: ACCEPT, REJECT, or ALL"
+  type        = string
+  default     = "ALL"
+}
+
+variable "flow_logs_destination_type" {
+  description = "Flow logs destination type: \"cloud-watch-logs\" or \"s3\""
+  type        = string
+  default     = "cloud-watch-logs"
+}
+
+variable "flow_logs_destination_arn" {
+  description = "ARN of the destination (CloudWatch Log Group ARN or S3 bucket ARN) for flow logs"
+  type        = string
+  default     = ""
+}
+
+variable "enable_s3_endpoint" {
+  description = "Create a gateway VPC endpoint for S3"
+  type        = bool
+  default     = false
+}
+
+variable "enable_dynamodb_endpoint" {
+  description = "Create a gateway VPC endpoint for DynamoDB"
+  type        = bool
+  default     = false
+}
+
+variable "enable_network_acl" {
+  description = "Whether to create basic network ACLs for public and private subnets"
   type        = bool
   default     = false
 }
