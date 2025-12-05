363b2dd6732774f10ac43a9742c3173e162b5d8b
alok raja <alokraja075@gmail.com>
2025-12-05 23:01:42 +0530

    add outputs for flow log ID, S3 and DynamoDB VPC endpoint IDs, and network ACL IDs

--- modules/vpc/outputs.tf
+++ modules/vpc/outputs.tf
@@ -15,5 +15,30 @@ output "private_subnet_ids" {
 
 output "nat_gateway_ids" {
   description = "List of NAT gateway IDs (empty if not created)"
-  value       = var.enable_nat ? [for n in aws_nat_gateway.nat : n.id] : []
+  value       = var.nat_gateway_strategy == "none" ? [] : [for n in aws_nat_gateway.nat : n.id]
+}
+
+output "flow_log_id" {
+  description = "Flow Log ID (if created)"
+  value       = length(aws_flow_log.this) > 0 ? aws_flow_log.this[0].id : ""
+}
+
+output "s3_endpoint_id" {
+  description = "S3 VPC endpoint ID (if created)"
+  value       = length(aws_vpc_endpoint.s3) > 0 ? aws_vpc_endpoint.s3[0].id : ""
+}
+
+output "dynamodb_endpoint_id" {
+  description = "DynamoDB VPC endpoint ID (if created)"
+  value       = length(aws_vpc_endpoint.dynamodb) > 0 ? aws_vpc_endpoint.dynamodb[0].id : ""
+}
+
+output "public_network_acl_id" {
+  description = "Public network ACL id (if created)"
+  value       = length(aws_network_acl.public) > 0 ? aws_network_acl.public[0].id : ""
+}
+
+output "private_network_acl_id" {
+  description = "Private network ACL id (if created)"
+  value       = length(aws_network_acl.private) > 0 ? aws_network_acl.private[0].id : ""
 }
