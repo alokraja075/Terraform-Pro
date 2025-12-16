505e55ca99ca6598536cc57f567b0e48d633d470
Alok Raja <94987623+alokraja075@users.noreply.github.com>
2025-12-16 19:07:21 +0000

    Add variables for security groups, launch templates, and autoscaling groups

--- EC2 Module/modules/ec2/variables.tf
+++ EC2 Module/modules/ec2/variables.tf
@@ -1,3 +1,56 @@
+variable "security_groups" {
+  description = "Map of security groups to create"
+  type = map(object({
+    description   = string
+    ingress_ports = set(number)
+  }))
+  default = {
+    web = {
+      description   = "Allow SSH and HTTP"
+      ingress_ports = [22, 80, 8000, 443]
+    }
+  }
+}
+
+variable "launch_templates" {
+  description = "Map of launch templates to create"
+  type = map(object({
+    ami           = string
+    instance_type = string
+    user_data     = string
+    sg_key        = string
+  }))
+  default = {
+    web = {
+      ami           = ""
+      instance_type = ""
+      user_data     = ""
+      sg_key        = "web"
+    }
+  }
+}
+
+variable "autoscaling_groups" {
+  description = "Map of autoscaling groups to create"
+  type = map(object({
+    lt_key                = string
+    min_size              = number
+    max_size              = number
+    desired_capacity      = number
+    subnet_ids            = list(string)
+    name                  = string
+  }))
+  default = {
+    web = {
+      lt_key           = "web"
+      min_size         = 2
+      max_size         = 3
+      desired_capacity = 2
+      subnet_ids       = []
+      name             = ""
+    }
+  }
+}
 variable "name" {
   type    = string
   default = "app"
