eca298142125fa21ce0c8787a055f8438b830fe0
Alok Raja <94987623+alokraja075@users.noreply.github.com>
2026-01-07 18:25:11 +0000

    Fix security group configuration in EKS cluster resource

--- EKS Module/modules/eks/main.tf
+++ EKS Module/modules/eks/main.tf
@@ -7,7 +7,7 @@ resource "aws_eks_cluster" "main" {
 
   vpc_config {
     subnet_ids              = var.subnet_ids
-    security_groups         = var.security_group_ids
+    security_group_ids      = var.security_group_ids
     endpoint_private_access = var.endpoint_private_access
     endpoint_public_access  = var.endpoint_public_access
   }
